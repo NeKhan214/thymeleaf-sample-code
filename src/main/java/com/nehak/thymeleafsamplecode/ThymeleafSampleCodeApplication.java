eb11745973bd6abf2113df51505dc95c34b4a5bc
NehaK214 <er.nehakhan@gmail.com>
2024-04-07 21:27:51 +0530

    Init commit

---
+++ src/main/java/com/nehak/thymeleafsamplecode/ThymeleafSampleCodeApplication.java
@@ -0,0 +1,13 @@
+package com.nehak.thymeleafsamplecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSampleCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafSampleCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nehak/thymeleafsamplecode/ThymeleafSampleCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nehak.thymeleafsamplecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSampleCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
